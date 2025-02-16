07faaa465141d216ada9d548a4b1790780bd5630
1itvin <litvin.roman2003@gmail.com>
2025-02-16 16:00:11 +0300

    feat(app-component): add user-info & more-info-button components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { MoreInfoButtonComponent } from './shared/more-info-button/more-info-button.component';
+import { UserInfoComponent } from './shared/user-info/user-info.component';
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MoreInfoButtonComponent, UserInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
