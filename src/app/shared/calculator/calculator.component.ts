20fc37c5cf435f68213ec8b969cb8d17187758bd
1itvin <litvin.roman2003@gmail.com>
2025-03-01 17:23:28 +0300

    feat(calculator-component): add operations & displayValues enums

--- src/app/shared/calculator/calculator.component.ts
+++ src/app/shared/calculator/calculator.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 import { ButtonConfig } from '../interfaces/button-config.interface';
 import { CalculatorButtonsDirective } from '../calculator-buttons.directive';
 import { CalculatorDisplayComponent } from './calculator-display/calculator-display.component';
-import { CommonModule } from '@angular/common';
+import { DisplayValues } from '../enums/displayValues.enum';
+import { Operations } from '../enums/operations.enum';
 
 @Component({
   selector: 'app-calculator',
@@ -13,9 +15,7 @@ import { CommonModule } from '@angular/common';
 
     // directives
     CalculatorButtonsDirective,
-
-    // modules
-    CommonModule,
+    NgClass,
   ],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss',
@@ -23,103 +23,103 @@ import { CommonModule } from '@angular/common';
 export class CalculatorComponent {
   public buttonsConfig: ButtonConfig[] = [
     {
-      operation: 'toggle-theme',
-      displayValue: '🌓',
+      operation: Operations.ToggleTheme,
+      displayValue: DisplayValues.ToggleTheme,
       handleClick: () => this.handleToggleTheme(),
     },
     {
-      operation: 'all-clear',
-      displayValue: 'AC',
+      operation: Operations.AllClear,
+      displayValue: DisplayValues.AllClear,
       handleClick: () => this.handleAllClear(),
     },
     {
-      operation: 'clear',
-      displayValue: 'C',
+      operation: Operations.Clear,
+      displayValue: DisplayValues.Clear,
       handleClick: () => this.handleClear(this.currentValue),
     },
     {
-      operation: 'percent',
-      displayValue: '%',
+      operation: Operations.Percent,
+      displayValue: DisplayValues.Percent,
       handleClick: () => this.appendSymbol('%'),
     },
     {
-      operation: 'digit7',
-      displayValue: '7',
+      operation: Operations.Digit7,
+      displayValue: DisplayValues.Digit7,
       handleClick: () => this.appendSymbol('7'),
     },
     {
-      operation: 'digit8',
-      displayValue: '8',
+      operation: Operations.Digit8,
+      displayValue: DisplayValues.Digit8,
       handleClick: () => this.appendSymbol('8'),
     },
     {
-      operation: 'digit9',
-      displayValue: '9',
+      operation: Operations.Digit9,
+      displayValue: DisplayValues.Digit9,
       handleClick: () => this.appendSymbol('9'),
     },
     {
-      operation: 'divide',
-      displayValue: '/',
+      operation: Operations.Divide,
+      displayValue: DisplayValues.Divide,
       handleClick: () => this.appendSymbol('/'),
     },
     {
-      operation: 'digit4',
-      displayValue: '4',
+      operation: Operations.Digit4,
+      displayValue: DisplayValues.Digit4,
       handleClick: () => this.appendSymbol('4'),
     },
     {
-      operation: 'digit5',
-      displayValue: '5',
+      operation: Operations.Digit5,
+      displayValue: DisplayValues.Digit5,
       handleClick: () => this.appendSymbol('5'),
     },
     {
-      operation: 'digit6',
-      displayValue: '6',
+      operation: Operations.Digit6,
+      displayValue: DisplayValues.Digit6,
       handleClick: () => this.appendSymbol('6'),
     },
     {
-      operation: 'multiply',
-      displayValue: '*',
+      operation: Operations.Multiply,
+      displayValue: DisplayValues.Multiply,
       handleClick: () => this.appendSymbol('*'),
     },
     {
-      operation: 'digit1',
-      displayValue: '1',
+      operation: Operations.Digit1,
+      displayValue: DisplayValues.Digit1,
       handleClick: () => this.appendSymbol('1'),
     },
     {
-      operation: 'digit2',
-      displayValue: '2',
+      operation: Operations.Digit2,
+      displayValue: DisplayValues.Digit2,
       handleClick: () => this.appendSymbol('2'),
     },
     {
-      operation: 'digit3',
-      displayValue: '3',
+      operation: Operations.Digit3,
+      displayValue: DisplayValues.Digit3,
       handleClick: () => this.appendSymbol('3'),
     },
     {
-      operation: 'subtract',
-      displayValue: '-',
+      operation: Operations.Subtract,
+      displayValue: DisplayValues.Subtract,
       handleClick: () => this.appendSymbol('-'),
     },
     {
-      operation: 'digit0',
-      displayValue: '0',
+      operation: Operations.Digit0,
+      displayValue: DisplayValues.Digit0,
       handleClick: () => this.appendSymbol('0'),
     },
     {
-      operation: 'point',
-      displayValue: '.',
+      operation: Operations.Point,
+      displayValue: DisplayValues.Point,
       handleClick: () => this.appendSymbol('.'),
     },
     {
-      operation: 'equals',
-      displayValue: '=',
+      operation: Operations.Equals,
+      displayValue: DisplayValues.Equals,
       handleClick: () => this.appendSymbol('='),
     },
     {
-      operation: 'add',
-      displayValue: '+',
+      operation: Operations.Add,
+      displayValue: DisplayValues.Add,
       handleClick: () => this.appendSymbol('+'),
     },
   ];
