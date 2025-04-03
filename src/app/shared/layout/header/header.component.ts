b27f84e6196cd6d2b8017ff9c7c557ab766d28ce
1itvin <ya.roman.litvin@yandex.by>
2025-04-03 15:40:20 +0300

    feat(ui): integrate book adding dialog with button "add new"

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { AddBookDialogComponent } from '../../dialogs/books/add-book-dialog/add-book-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +9,10 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private dialog = inject(MatDialog);
+
+  public openAddDialog(): void {
+    this.dialog.open(AddBookDialogComponent);
+  }
+}
