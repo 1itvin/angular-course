445e31e9b9a0e803aee6688a778de470fd0a7d46
1itvin <ya.roman.litvin@yandex.by>
2025-04-03 17:09:40 +0300

    feat(alert): add toastr for book management notifications

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,37 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { IndividualConfig, provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { authorizationInterceptor } from './core/interceptors/authorization.interceptor';
 import { routes } from './app.routes';
 
+const toastrConfig: Partial<IndividualConfig> = {
+  closeButton: true, // Показывать кнопку закрытия
+  timeOut: 5_000, // Время жизни уведомления в миллисекундах
+  extendedTimeOut: 1_000, // Дополнительное время при наведении мыши
+  disableTimeOut: false, // Тайм-аут включён
+  easing: 'ease-in', // Ускорение анимации
+  easeTime: 300, // Время анимации в миллисекундах
+  enableHtml: true, // Разрешить HTML в сообщении
+  newestOnTop: true, // Новые уведомления размещаются сверху
+  progressBar: true, // Показывать прогресс-бар
+  progressAnimation: 'decreasing', // Убывающая анимация прогресс-бара
+  toastClass: 'ngx-toastr', // CSS класс для тостов //
+  positionClass: 'toast-top-right', // Позиция уведомления на экране
+  titleClass: 'toast-title', // CSS класс для заголовков //
+  messageClass: 'toast-message', // CSS класс для сообщений //
+  tapToDismiss: true, // Закрытие уведомления при клике //
+  onActivateTick: false, // Не требуется ручное обновление в ChangeDetectorRef //
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideAnimations(),
     provideHttpClient(withInterceptors([authorizationInterceptor])),
+    provideRouter(routes),
+    provideToastr(toastrConfig),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 import { AddBookDialogComponent } from '../../dialogs/books/add-book-dialog/add-book-dialog.component';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +11,33 @@ import { AddBookDialogComponent } from '../../dialogs/books/add-book-dialog/add-
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy, OnInit {
   private dialog = inject(MatDialog);
+  private dialogRefSub!: Subscription;
+  private toastr = inject(ToastrService);
 
   public openAddDialog(): void {
     this.dialog.open(AddBookDialogComponent);
+  }
+
+  public ngOnInit(): void {
+    this.dialogRefSub = this.dialog.afterAllClosed.subscribe(() => {
+      this.showError();
+      this.showSuccess();
+    });
+  }
+
+  public showSuccess(): void {
+    this.toastr.success('Книга успешно добавлена!');
+  }
+
+  public showError(): void {
+    this.toastr.error('Ошибка при добавлении книги');
+  }
+
+  public ngOnDestroy(): void {
+    if (this.dialogRefSub) {
+      this.dialogRefSub.unsubscribe();
+    }
   }
 }
