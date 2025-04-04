9905fb0b86a240f0991d405adc3ecee7c2bbc9ab
1itvin <ya.roman.litvin@yandex.by>
2025-04-04 15:25:32 +0300

    feat(routes): add home & categories routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { HomeComponent } from './pages/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'home', component: HomeComponent },
+];
