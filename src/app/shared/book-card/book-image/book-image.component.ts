f8bb59ecccb667a87238faf2254be5ff604267de
1itvin <ya.roman.litvin@yandex.by>
2025-04-04 17:11:46 +0300

    feat(book-card): add book image component

---
+++ src/app/shared/book-card/book-image/book-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-image',
+  imports: [],
+  templateUrl: './book-image.component.html',
+  styleUrl: './book-image.component.scss'
+})
+export class BookImageComponent {
+
+}
