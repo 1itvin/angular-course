6a49edf2afd20400e8d5cfc46d997396d7adbd53
1itvin <litvin.roman2003@gmail.com>
2025-04-10 22:47:39 +0300

    feat(home): use upgraded book-card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { BookCardComponent } from "../../shared/book-card/book-card.component";
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { Book } from '../../core/models/book.type';
+import { BookCardComponent } from '../../shared/book-card/book-card.component';
+import { BookService } from './../../core/services/book.service';
 
 @Component({
   selector: 'app-home',
-  imports: [BookCardComponent],
+  imports: [
+    // components
+    BookCardComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public books: Book[] = [];
 
+  private readonly bookService = inject(BookService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  public ngOnInit() {
+    this.bookService
+      .getAllBooks()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((books) => {
+        this.books = books;
+      });
+  }
 }
