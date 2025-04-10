d3a3d9bfa7f4d16713f32bba9bde03a61422f02b
1itvin <litvin.roman2003@gmail.com>
2025-04-10 22:41:38 +0300

    feat(book-image): add @Input() to support custom images

--- src/app/shared/book-card/book-image/book-image.component.ts
+++ src/app/shared/book-card/book-image/book-image.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-book-image',
   imports: [],
   templateUrl: './book-image.component.html',
-  styleUrl: './book-image.component.scss'
+  styleUrl: './book-image.component.scss',
 })
 export class BookImageComponent {
+  @Input() imageSource: string | undefined;
 
+  public imageNotAvailable =
+    'https://www.booklineservices.com/v1/image/cache/catalog/product/Book%202/NO%20IMAGE-600x770.jpg';
 }
