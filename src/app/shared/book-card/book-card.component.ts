62e3a1e6c2effc23d28a6fb0a76d0d7fbe43d13a
1itvin <litvin.roman2003@gmail.com>
2025-04-10 22:47:03 +0300

    feat(book-card): use new functionalities from @Input`s

--- src/app/shared/book-card/book-card.component.ts
+++ src/app/shared/book-card/book-card.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookImageComponent } from './book-image/book-image.component';
+import { BookService } from '../../core/services/book.service';
 import { ButtonComponent } from '../custom-forms/button/button.component';
+import { EditBookDialogComponent } from '../dialogs/books/edit-book-dialog/edit-book-dialog.component';
 import { LikeComponent } from './like/like.component';
 
 @Component({
@@ -21,4 +24,23 @@ import { LikeComponent } from './like/like.component';
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.scss',
 })
-export class BookCardComponent {}
+export class BookCardComponent {
+  @Input() id!: string;
+  @Input() imageSource?: string;
+  @Input() title!: string;
+  @Input() publicationYear!: Date;
+  @Input() author?: string;
+
+  private readonly bookService = inject(BookService);
+  private readonly dialog = inject(MatDialog);
+
+  public openEditDialog(): void {
+    this.dialog.open(EditBookDialogComponent);
+  }
+
+  public delete() {
+    this.bookService.deleteBook(this.id).subscribe(() => {
+      console.log(`Книга с id ${this.id} успешно удалена`);
+    });
+  }
+}
